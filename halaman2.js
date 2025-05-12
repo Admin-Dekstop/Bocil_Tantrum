9f03bef2f1849a23c56358517b3e9f1b3b559510
FANDY <fandyitdesktop@gmail.com>
2025-05-12 19:31:38 +0800

    Update halaman2.js

--- halaman2.js
+++ halaman2.js
@@ -1,9 +1,17 @@
-const fotoEls = document.querySelectorAll(".foto");
-const kataEl = document.getElementById("kataRomantis");
+// Function to show popup message when photo is clicked
+const photos = document.querySelectorAll('.photo');
+const popup = document.getElementById('popup');
+const popupText = document.getElementById('popupText');
 
-fotoEls.forEach(foto => {
-  foto.addEventListener("click", () => {
-    const pesan = foto.getAttribute("data-kata");
-    kataEl.textContent = pesan;
+photos.forEach(photo => {
+  photo.addEventListener('click', function () {
+    const message = photo.getAttribute('data-message');
+    popupText.textContent = message;
+    popup.classList.remove('hidden');
   });
 });
+
+// Function to close the popup
+function closePopup() {
+  popup.classList.add('hidden');
+}
